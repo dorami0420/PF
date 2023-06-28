62ffb90fe9d5929df204defe6f18b716b674af5e
Miwako <you@example.com>
2023-06-28 02:57:46 +0000

    W

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,7 @@
+class AdminController < ApplicationController
+  def top
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'admin/top'
+  get 'admin/edit'
+  get 'homes/top'
+  get 'homes/about'
+  get 'users/new'
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
