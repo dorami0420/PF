b2868a00f2e9ec54ebb78d024e10480bbecbdf24
Miwako <you@example.com>
2023-06-29 12:49:18 +0000

    W

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,23 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top]
+ # before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-     post_images_path
+    if resource.class == Admin
+     admin_root_path
+   else
+     root_path
+   end
   end
 
 
   def after_sign_out_path_for(resource)
-    about_path
+    if resource == :admin
+      new_admin_session_path
+    else
+      root_path
+    end
   end
-
 
   protected
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,32 @@
 Rails.application.routes.draw do
   
-   devise_for :admin, skip: [:registrations, :passwords], controllers: {
-   sessions: "admin/sessions",
+
+  devise_for :admins, path: 'admin', skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/admins/sessions",
   }
 
-
-  devise_for :users,skip: [:passwords], controllers: {
-    registrations: "user/registrations",
-    sessions: "user/sessions",
+  devise_for :users, skip: [:passwords], controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
   }
   
-  
-   namespace :user do
-    root to: "homes#top"
-    get "homes/about"
-    get 'index'
-    get "information/edit" => "user#edit", as: "user_edit"
-    get "mypage" => "user#show", as: "user_mypage"
-    patch "update" => "user#update"
-    get "user/confirm" => "user#confirm", as: "user_confirm"
-    patch "user/cancel" => "user#cancel", as: "user_cancel"
-   end
-     #resources :users, only: [:index, :show, :edit, :update]
-    resources :stray_cats, only: [:new, :index, :update, :destroy, :create, :edit] do
-    resources :protective_cats, only: [:new, :index, :update, :destroy, :create, :edit]
+  root to: "homes#top"
+  get "homes/about"
+    
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :stray_cats do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
-   end
-
-
-  namespace :admin do
-    get 'post_images/index'
-
-    get "top" => "homes#top", as: "top"
-    get "show" => "homes#show", as: "show"
-
-   resources :stray_cats, only: [:new, :index, :update, :destroy, :create, :edit] do
-    resources :protective_cats, only: [:new, :index, :update, :destroy, :create, :edit]
-    resources :users, expect: [:new, :destroy, :create]
   end
- 
-   
 
- 
+  resources :protective_cats do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
   
+  namespace :admin do
+    root to: "home#top"
+    resources :users, only: [:index, :destroy]
+  end
+end
 
-   
-  
-  
-  
-  
-  
-   
- 
-
-  
