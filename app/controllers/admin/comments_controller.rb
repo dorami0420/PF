df3fab2ef36abbc941340b5863e0c529dc27133e
Miwako <you@example.com>
2023-07-17 13:39:14 +0000

    W

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,4 +1,12 @@
 class Admin::CommentsController < ApplicationController
   def index
+    @comments = Comment.all
   end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    redirect_to '/comments'
+  end
+
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -19,14 +19,14 @@ end
 def destroy
     user = User.find(params[:id])  
     user.destroy
-    redirect_to '/lists'  
+    redirect_to '/users'  
 end
   
 private
 
-def user_params
-  params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
-end
+# def user_params
+#   params.require(:user).permit(:first_name, :last_name, :first_name_kana, :last_name_kana, :postal_code, :address, :telephone_number)
+# end
 
 
 end
